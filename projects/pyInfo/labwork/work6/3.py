67c2a19174c22501fb773ef63caa64feac981b77
lomkiry <66978998+lomkiry@users.noreply.github.com>
2024-10-30 02:27:26 +0700

    Delete projects/pyInfo/labwork directory

--- projects/pyInfo/labwork/work6/3.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Введите n: "))
-arr = list(range(1, n+1))
-total = 0
-for i in arr:
-    total = total + i*i
-
-print("Ваше число:", total)
--- projects/pyInfo/labwork/work6/8.py
+++
@@ -1,9 +0,0 @@
-b = input("Введите двоичное число: ")
-step = 0
-total = 0
-
-for i in range(len(b)-1, -1, -1):
-    if b[i] == '1':
-        total = total + 2**step
-    step+=1
-print(total)
